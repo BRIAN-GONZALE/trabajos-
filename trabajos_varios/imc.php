1df0b2391a0864b5219e93f9b38bb3c5fc1f835f
brian <brian.gonzalez@educacionadventista.org.ar>
2024-08-11 11:00:20 -0300

    mi proyecto

---
+++ trabajos_varios/imc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../operadores_aritmeticos_php/img/file-code-solid.svg" type="image/x-icon">
+    <title>IMC</title>
+</head>
+<body>
+    <h1>
+        hola mundo
+    </h1>
+</body>
+</html>
